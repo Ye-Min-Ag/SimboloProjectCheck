32dbdf2fac74af858d01eb48a8252d0c36dc4c18
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-09-01 16:41:18 +0630

    Update regression.py

--- regression.py
+++ regression.py
@@ -23,31 +23,36 @@ if uploaded_file is not None:
 import streamlit as st
 import pandas as pd
 import pickle
-from io import BytesIO
-import requests
-
+model = pickle.load(open('trained_model.pkl','rb'))
 # Set title of the app
-st.title('ML Prediction App')
+st.set_page_config(
+    page_title="Prediction App",
+    page_icon="🤖",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
 
-# Upload a .csv file
-uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
-response = requests.get("https://github.com/Ye-Min-Ag/SimboloProjectCheck/raw/main/trained_model.pkl")
-model_content = response.content
-# Load the trained model using pickle
-model = pickle.loads(model_content)
+def predict_value(f):
+    input = f
+    prediction = model.predict(input)
+    return prediction
+
+def main():
+    # Upload a .csv file
+    uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+    if uploaded_file is not None:
+        feature = feature_extraction(uploaded_file)
+        Predicted_values = predict_value(feature)
 
 # Display the prediction form if a file is uploaded
-if uploaded_file is not None:
+def feature_extraction(file):
     # Read the uploaded .csv file
-    data = pd.read_csv(uploaded_file)
+    data = pd.read_csv(file)
     file_X = data.iloc[:, 0:-1].values
     file_Y = data.iloc[:, -1].values
-
-    # Make predictions using the model
-    predictions = model.predict(file_X)
+    return file_X
 
     # Display the predictions and true values
-    st.write('Predictions:')
-    st.write(predictions)
-    st.write('True values:')
-    st.write(file_Y)
+st.write('Predictions:')
+st.write(Predicted_values)
+
