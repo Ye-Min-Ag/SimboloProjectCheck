d3b8494fd229dfcb93ac804e797a2043104c3431
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-09-01 10:56:09 +0630

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pandas as pd
-#import joblib
-#from io import BytesIO
-import requests
 import pickle
+from io import BytesIO
+import requests
 
 st.title('ML Prediction App')
 uploaded_file = st.file_uploader('Upload an CSV file', type=['csv'])
 response = requests.get("https://github.com/Ye-Min-Ag/SimboloProjectCheck/blob/main/trained_model.pkl")
-content = response.content
-model = pickle.load(open(content,'rb')) 
+model_content = response.content
+model = pickle.loads(model_content)
 if uploaded_file is not None:
     # Read the uploaded .xlsx file
     data = pd.read_csv(uploaded_file)
