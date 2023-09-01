82b95a86977d88627151f07d22b1891f6ff9d7b4
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-09-01 09:54:03 +0630

    Update regression.py

--- regression.py
+++ regression.py
@@ -2,19 +2,19 @@ import streamlit as st
 import pandas as pd
 import joblib
 from io import BytesIO
-import requests
+#import requests
 
 # Set title of the app
 st.title('ML Prediction App')
 
 # Upload a .xlsx file
 uploaded_file = st.file_uploader('Upload an CSV file', type=['csv'])
-response = requests.get("https://github.com/Ye-Min-Ag/SimboloProjectCheck/blob/main/trained_model.pkl")
-content = response.content
-print(type(content))
-buffer = BytesIO(content)
+#response = requests.get("https://github.com/Ye-Min-Ag/SimboloProjectCheck/blob/main/trained_model.pkl")
+#content = response.content
+#print(type(content))
+#buffer = BytesIO(content)
 # Load the trained model
-model = joblib.load(buffer) 
+model = joblib.load("https://github.com/Ye-Min-Ag/SimboloProjectCheck/blob/main/trained_model.pkl") 
 
 # Display the prediction form if a file is uploaded
 if uploaded_file is not None:
